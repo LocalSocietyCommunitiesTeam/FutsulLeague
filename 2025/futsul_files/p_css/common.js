d4199495505c78d244aa0a19c9ae6ca840120a39
UniqueSeaWeb <uniquesea912@gmail.com>
2025-09-27 13:47:34 +0900

    メンバー登録　少し編集

--- 2025/futsul_files/p_css/common.js
+++ 2025/futsul_files/p_css/common.js
@@ -2,10 +2,13 @@
 var LEAGUE_NUMBER = 2;
 
 window.addEventListener('DOMContentLoaded', function () {
-    const reloadBtn = document.getElementById('com_reloadBtn');
+    // 更新ボタン押下時の処理
+    if (document.getElementById('com_reloadBtn')) {
+        const reloadBtn = document.getElementById('com_reloadBtn');
 
-    // 更新ボタンを押下
-    reloadBtn.addEventListener('click', function () {
-        location.reload();
-    });
+        // 更新ボタンを押下に画面をリロード
+        reloadBtn.addEventListener('click', function () {
+            location.reload();
+        });
+    }
 });
