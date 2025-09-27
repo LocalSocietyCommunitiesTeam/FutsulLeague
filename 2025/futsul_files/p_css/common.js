1dbf45e0ff9d3411e60f498a07857c343cdba5c0
UniqueSeaWeb <uniquesea912@gmail.com>
2025-09-27 23:05:34 +0900

    メンバー編集

--- 2025/futsul_files/p_css/common.js
+++ 2025/futsul_files/p_css/common.js
@@ -1,6 +1,19 @@
 /** 画面共通 **/
 var LEAGUE_NUMBER = 2;
 
+// クエリストリングのvalueを取得する関数
+function getQuery(key) {
+    const params = new URLSearchParams(window.location.search);
+    const values = params.getAll(key);
+    if (values.length === 0) {
+        return null;
+    } else if (values.length === 1) {
+        return values[0];
+    } else {
+        return values;
+    }
+}
+
 window.addEventListener('DOMContentLoaded', function () {
     // 更新ボタン押下時の処理
     if (document.getElementById('com_reloadBtn')) {
--- 2025/futsul_files/p_css/member_register.js
+++ 2025/futsul_files/p_css/member_register.js
@@ -1,1 +1,10 @@
 /** メンバー登録 **/
+window.addEventListener('DOMContentLoaded', function() {
+    if (getQuery('type') == 'edit') {
+        console.log('編集モード');
+        const title = document.getElementById('mer_title');
+        const exp = document.getElementById('mer_exp');
+        title.innerText = '編集';
+        exp.innerText = '参加メンバーを編集してください。';
+    }
+});
